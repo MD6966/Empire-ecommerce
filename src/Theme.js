9d56ac5577652b60d6b98cf6c8c46734fca8963f
Syed Mudasser Anayat <107185992+MD6966@users.noreply.github.com>
2023-09-11 10:39:34 +0500

    first-commit

---
+++ src/Theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+          main: '#000', 
+        },
+        secondary: {
+          main: '#2DC799', // Your secondary color
+        },
+      },
+    
+});
